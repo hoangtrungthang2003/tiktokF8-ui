475e7f6e10c7fa7c03cc3562f186ea5eb4b299d8
hoangtrungthang2003 <hoangtrungthang2003@gmail.com>
2024-08-16 20:10:56 +0700

    Dựng khung layout mặc định

---
+++ src/routes/index.js
@@ -0,0 +1,24 @@
+// Layouts
+import {HeaderOnly} from '~/components/Layout';
+
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+import { Component } from 'react';
+
+//Public routes
+const publicRoutes = [
+    { path:'/', component: Home },
+    { path:'/home', component: Home },
+    { path:'/following', component: Following },
+    { path:'/profile', component: Profile },
+    { path:'/upload', component: Upload, layout: HeaderOnly },
+    { path:'/search', component: Search, layout: null },
+    
+]
+
+const privateRoutes = []
+
+export { publicRoutes, privateRoutes}
